c146a3b4f13df4734f72b5d487dc2e1482ffdfc1
Amrat <amratamrat2000@gmail.com>
2025-01-31 18:02:47 +0500

    Leetcode problems solutions

---
+++ src/Trees/BST/KthSmallestElementInBST.java
@@ -0,0 +1,27 @@
+package Trees.BST;
+
+// problem
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+public class KthSmallestElementInBST {
+
+//    int count = 0;
+//    public int kthSmallest(Node root, int k){
+//        return findKthSmallest(root, k).val;
+//    }
+//    private Node findKthSmallest(Node root, int k){
+//        if (root == null){
+//            return null;
+//        }
+//        Node left = findKthSmallest(root.left, k);
+//        if (left != null){
+//            return left;
+//        }
+//        count++;
+//        if (count == k){
+//            return root;
+//        }
+//        return findKthSmallest(root.right, k);
+//    }
+
+}
