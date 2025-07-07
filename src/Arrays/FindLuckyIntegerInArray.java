9e5aa08df361b679b2b2e79841b67db036858e23
Amrat <amratamrat2000@gmail.com>
2025-07-08 00:50:11 +0500

    find lucky integer problem solution

---
+++ src/Arrays/FindLuckyIntegerInArray.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindLuckyIntegerInArray {
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,3,3,4,5,5,5,5};
+        System.out.println(findLucky(arr));
+    }
+
+    public static int findLucky(int[] arr){
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int j : arr) {
+            Integer occ = freq.get(j);
+            if (occ != null) {
+                occ += 1;
+                freq.put(j, occ);
+            } else {
+                freq.put(j, 1);
+            }
+        }
+        int ans = -1;
+        Set<Integer> keys = freq.keySet();
+        for(Integer key: keys){
+            int occ = freq.get(key);
+            if (occ == key && key > ans){
+                ans = key;
+            }
+        }
+        return ans;
+    }
+}
